53fbf3345abc10a7911758b01ea92a835b3db72d
seufster71 <seufster@gmail.com>
2022-07-11 10:02:01 -0400

    update gitpull

--- stockanalyzer/gitpullfile.sh
+++ stockanalyzer/gitpullfile.sh
@@ -1,5 +1,5 @@
 GIT_DIR="gitanalyzer"
-BRANCH="Ed_1_on_develop"
+BRANCH="feature/Ed_1_on_develop"
 
 
 ECHO admin
@@ -18,9 +18,14 @@ ECHO coreView
 cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/coreView
 git pull origin $BRANCH
 
-ECHO member dashboard
-cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/member/dashboard
+ECHO system
+cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/system
 git pull origin $BRANCH
+
+ECHO systemView
+cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/systemView
+git pull origin $BRANCH
+
 
 ECHO member logout
 cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/member/logout
@@ -34,14 +39,6 @@ ECHO member session
 cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/member/session
 git pull origin $BRANCH
 
-ECHO member submenu
-cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/member/submenu
-git pull origin $BRANCH
-
-ECHO memberView dashboard
-cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/memberView/dashboard
-git pull origin $BRANCH
-
 ECHO memberView logout
 cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/memberView/logout
 git pull origin $BRANCH
@@ -50,17 +47,7 @@ ECHO memberView profile
 cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/memberView/profile
 git pull origin $BRANCH
 
-ECHO memberView submenu
-cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/memberView/submenu
-git pull origin $BRANCH
 
-ECHO system
-cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/system
-git pull origin $BRANCH
-
-ECHO systemView
-cd ~/$GIT_DIR/stockanalyzer/stockanalyzer/src/main/js/systemView
-git pull origin $BRANCH
 
 ECHO main
 cd ~/$GIT_DIR/stockanalyzer/stockanalyzer
